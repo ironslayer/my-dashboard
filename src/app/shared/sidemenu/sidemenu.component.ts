dc1857fed5e5cef1c87298ad202861f336c0f4ca
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-05-25 19:49:05 -0400

    feat: refactor sidemenu component and add new heavy loaders components

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { routes } from '../../../app.routes';
+import { routes } from '../../app.routes';
 import { RouterModule } from '@angular/router';
 
 @Component({
