232b74b79e846b2a6d9a5cc8676f047fcbacd964
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-05-25 03:22:29 -0400

    feat: implement SidemenuComponent structure and integrate routing

--- src/app/dashboard/shared/sidemenu/sidemenu.component.ts
+++ src/app/dashboard/shared/sidemenu/sidemenu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { routes } from '../../../app.routes';
 
 @Component({
   selector: 'app-sidemenu',
@@ -7,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrl: './sidemenu.component.css'
 })
 export class SidemenuComponent {
+
+  public menuItems = routes
+    .map( route => route.children ?? [])
+    .flat()
+    .filter(route => route && route.path)
+    .filter(route => !route.path?.includes(':'))
+
+  constructor() {
+    // const dashboardRoutes = routes.map(
+    //   route => route.children ?? []
+    // )
+    //   .flat()
+    //   .filter(route => route && route.path)
+    //   .filter(route => !route.path?.includes(':'))
+
+    // console.log(dashboardRoutes)
+  }
+
 
 }
