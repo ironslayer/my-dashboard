f55c3a0d8a8baa29a9b83e275e3710c7711cad67
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-05-26 04:09:03 -0400

    feat: enhance UsersComponent with dependency injection for UsersService; add TitleComponent and RouterModule imports; remove unused ViewTransitionComponent and its template

--- src/app/dashboard/pages/users/users.component.ts
+++ src/app/dashboard/pages/users/users.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
+import { RouterModule } from '@angular/router';
+import { UsersService } from '@services/users.service';
 
 @Component({
   selector: 'app-list-users',
-  imports: [],
+  imports: [TitleComponent, RouterModule],
   templateUrl: './users.component.html',
   styles: ``
 })
 export default class UsersComponent {
+  public usersServices = inject(UsersService);
+
 
 }
