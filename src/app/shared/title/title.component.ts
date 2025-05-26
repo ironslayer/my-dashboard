6bc62cf3690b48f2f85adcc31557ae587401e1fe
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-05-25 23:46:09 -0400

    feat: add TitleComponent and integrate it into ControlFlowComponent

--- src/app/shared/title/title.component.ts
+++ src/app/shared/title/title.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { booleanAttribute, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   imports: [],
-  templateUrl: './title.component.html',
-  styles: ``
+  template: `
+    <h1 class="text-3xl mb-5"> {{ title }} - {{ withShadow }} </h1>
+  `,
 })
 export class TitleComponent {
+  
+  @Input({ required: true })
+  public title!: string;
+
+  @Input({ transform: booleanAttribute})
+  public withShadow: boolean = false;
 
 }
