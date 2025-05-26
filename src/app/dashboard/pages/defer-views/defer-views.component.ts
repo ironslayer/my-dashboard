4ae400f796a86381166db1dea8a4ada0da7423f4
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-05-26 04:08:47 -0400

    feat: update DeferViewsComponent to include HeavyLoadersSlowComponent and TitleComponent; remove unused user component template

--- src/app/dashboard/pages/defer-views/defer-views.component.ts
+++ src/app/dashboard/pages/defer-views/defer-views.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { HeavyLoadersSlowComponent } from '@shared/heavy-loaders/heavy-loaders-slow.component';
+import { TitleComponent } from "../../../shared/title/title.component";
 
 @Component({
   selector: 'app-defer-views',
-  imports: [],
+  imports: [HeavyLoadersSlowComponent, TitleComponent],
   templateUrl: './defer-views.component.html',
-  styles: ``
+  
 })
 export default class DeferViewsComponent {
 
