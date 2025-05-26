f231777286260c86af1c74f761e6340878b8272a
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-05-26 04:08:54 -0400

    feat: enhance UserComponent with reactive user data fetching and dynamic title; update UsersComponent template for improved user listing

--- src/app/dashboard/pages/user/user.component.ts
+++ src/app/dashboard/pages/user/user.component.ts
@@ -1,11 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
+
+import { User } from '@interfaces/req-response';
+import { TitleComponent } from '@shared/title/title.component';
+import { switchMap } from 'rxjs';
+import { UsersService } from '@services/users.service';
 
 @Component({
   selector: 'app-user',
-  imports: [],
-  templateUrl: './user.component.html',
-  styles: ``
+  imports: [TitleComponent],
+  template:`
+    <app-title [title]="titleLabel()" />
+
+    @if (user()) {
+
+      <section>
+        <img 
+        [srcset]="user()!.avatar "
+        [alt]="user()!.first_name"
+        >
+
+        <div>
+          <h3>
+            {{ user()!.first_name }} {{ user()!.last_name }}
+          </h3>
+          <p>
+            {{ user()!.email }}
+          </p>
+        </div>
+
+
+      </section>
+
+    }@else {
+      <p>Cargando Información</p>
+    }
+  `,
 })
 export default class UserComponent {
+
+  private route = inject( ActivatedRoute );
+  private usersService = inject( UsersService );
+
+
+
+  // public user = signal<User|undefined>(undefined);
+  public user = toSignal(
+    this.route.params.pipe(
+      switchMap( ({ id }) => this.usersService.getUserById( id ) )
+    )
+  )
+
+  public titleLabel = computed( () => {
+    if ( this.user() ) {
+      return `Información del Usuario: ${this.user()!.first_name} ${this.user()!.last_name}`;
+    }
+    return 'Información del Usuario';
+  });
+  // constructor() {
+  //   console.log(this.route.params.subscribe( params => {
+  //     console.log({params});
+  //   }))
+  // }
 
 }
